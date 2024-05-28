d079f2bde6978b983f4528831c00ede74c91731f
mashen <mashen@student.ethz.ch>
2024-05-28 21:28:58 +0200

    Did some stuff

--- 9 to 5 simulator/Assets/Scripts/GameBehaviour.cs
+++ 9 to 5 simulator/Assets/Scripts/GameBehaviour.cs
@@ -1,9 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.UI;
 
 public class GameBehaviour : MonoBehaviour
-{
+{ 
+    public bool inShift = false;
+    public bool win = false;
+    [SerializeField]
+    private float timeSpentWorking = 0.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,9 @@ public class GameBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (inShift)
+        {
+            timeSpentWorking += Time.deltaTime;
+        }
     }
 }
