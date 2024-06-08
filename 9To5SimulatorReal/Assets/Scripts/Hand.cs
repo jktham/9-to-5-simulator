5fb0b34c1e645448a226535818b4610bc2514cf2
mashen <mashen@student.ethz.ch>
2024-06-08 23:50:07 +0200

    DSALGALSGHLJAHFG

--- 9To5SimulatorReal/Assets/Scripts/Hand.cs
+++ 9To5SimulatorReal/Assets/Scripts/Hand.cs
@@ -23,14 +23,14 @@ public class Hand : MonoBehaviour
     void OnTriggerEnter(Collider other) {
 
         // Highlight when interactable in reach
-        if (other.gameObject.CompareTag("Interactable") && !carry && checkHands(other)) {
+        if (other.gameObject.CompareTag("Interactable") && !carry) {
             Renderer handRender = this.gameObject.GetComponent<Renderer>();
             handRender.material.SetColor("_Color", Color.green);
             inRange = other;
         }
 
         // Highlight when special in reach
-        if (other.gameObject.CompareTag("Special") && !game.inShift && checkHands(other)) {
+        if (other.gameObject.CompareTag("Special") && !game.inShift && !carry) {
             Renderer handRender = this.gameObject.GetComponent<Renderer>();
             handRender.material.SetColor("_Color", Color.yellow);
             inRange = other;
@@ -41,39 +41,36 @@ public class Hand : MonoBehaviour
     void OnTriggerExit(Collider other) {
 
         // Undo highlight when out of reach
-        if (other.gameObject.CompareTag("Interactable") || other.gameObject.CompareTag("Special")) {
+        if ((other.gameObject.CompareTag("Interactable") || other.gameObject.CompareTag("Special")) && inRange == other) {
             resethand();
         }
 
-    }
-
-    void OnTriggerStay(Collider other) {
-
-        // Picking stuff up
-        if (other.gameObject.CompareTag("Interactable") && input.action.ReadValue<float>() == 1.0f 
-            && !carry && inRange == other)
-        {
-            this.gameObject.GetComponent<FixedJoint>().connectedBody = other.gameObject.GetComponent<Rigidbody>();
-
-            carry = true;
-
-        } else if (other.gameObject.CompareTag("Interactable") && input.action.ReadValue<float>() == 0.0f && carry) {
-
-            this.gameObject.GetComponent<FixedJoint>().connectedBody = null;
-            carry = false;
-            
-        }
-
-        // startGame
-        if (other.gameObject.CompareTag("Special") && input.action.ReadValue<float>() == 1.0f && inRange == other) {
-            game.startShift();
-            resethand();
-        }
     }
 
     void Start() {
         game = GameObject.Find("GameController").GetComponent<GameBehaviour>();
         otherHandControl = otherHand.GetComponent<Hand>();
+    }
+
+    void Update() {
+
+        // Interaction
+        if (input.action.ReadValue<float>() == 1.0f && inRange != null && !carry)
+        {
+            if (inRange.gameObject.CompareTag("Special")) {
+                game.startShift();
+                resethand();
+            } else if (inRange.gameObject.CompareTag("Interactable")) {
+                this.gameObject.GetComponent<FixedJoint>().connectedBody = inRange.gameObject.GetComponent<Rigidbody>();
+                carry = true;
+            }
+
+        } else if (input.action.ReadValue<float>() == 0.0f && inRange != null && carry) {
+
+            this.gameObject.GetComponent<FixedJoint>().connectedBody = null;
+            carry = false;
+            
+        }
     }
 
     // Resets color of hands
@@ -84,7 +81,7 @@ public class Hand : MonoBehaviour
     }
 
     bool checkHands(Collider other) {
-        return otherHandControl.inRange == null || (otherHandControl.inRange != other);
+        return (otherHandControl.inRange == null && inRange == null) || (otherHandControl.inRange != other);
     }
 
 }
