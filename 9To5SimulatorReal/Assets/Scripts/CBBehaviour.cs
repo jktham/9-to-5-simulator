6167fa1b1b04fd7bbf11b703acfc4c6387a26e4e
mashen <mashen@student.ethz.ch>
2024-06-08 23:16:34 +0200

    huiglzfkj

--- 9To5SimulatorReal/Assets/Scripts/CBBehaviour.cs
+++ 9To5SimulatorReal/Assets/Scripts/CBBehaviour.cs
@@ -15,7 +15,7 @@ public class CBBehaviour : MonoBehaviour
         // apply force to patties on conveyor belt
         if (other.gameObject.CompareTag("Interactable"))
         {
-            Vector3 force = new Vector3(conveyorForce * 100 * Time.deltaTime, 0f, 0f);
+            Vector3 force = new Vector3(conveyorForce * Time.deltaTime, 0f, 0f);
             other.gameObject.GetComponent<Rigidbody>().AddForce(force, ForceMode.VelocityChange);
         }
 
